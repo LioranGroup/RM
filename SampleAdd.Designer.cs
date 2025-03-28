2eecdaf0c93ca3f4b0afffe806232a4914e908cf
LioranGroup <esqcorp3@gmail.com>
2025-03-28 12:54:18 -0600

    Add files via upload

---
+++ SampleAdd.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace RM
+{
+    partial class SampleAdd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            label1 = new Label();
+            guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            CloseBttn = new Guna.UI2.WinForms.Guna2Button();
+            SaveBttn = new Guna.UI2.WinForms.Guna2Button();
+            guna2Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox1).BeginInit();
+            guna2Panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            guna2Panel1.Controls.Add(label1);
+            guna2Panel1.Controls.Add(guna2PictureBox1);
+            guna2Panel1.CustomizableEdges = customizableEdges3;
+            guna2Panel1.Dock = DockStyle.Top;
+            guna2Panel1.FillColor = Color.FromArgb(250, 255, 197);
+            guna2Panel1.Location = new Point(0, 0);
+            guna2Panel1.Name = "guna2Panel1";
+            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            guna2Panel1.Size = new Size(1158, 142);
+            guna2Panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(154, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(202, 72);
+            label1.TabIndex = 1;
+            label1.Text = "Header";
+            // 
+            // guna2PictureBox1
+            // 
+            guna2PictureBox1.BackColor = Color.Transparent;
+            guna2PictureBox1.CustomizableEdges = customizableEdges1;
+            guna2PictureBox1.ImageRotate = 0F;
+            guna2PictureBox1.Location = new Point(12, 12);
+            guna2PictureBox1.Name = "guna2PictureBox1";
+            guna2PictureBox1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2PictureBox1.Size = new Size(124, 106);
+            guna2PictureBox1.TabIndex = 0;
+            guna2PictureBox1.TabStop = false;
+            // 
+            // guna2Panel2
+            // 
+            guna2Panel2.Controls.Add(CloseBttn);
+            guna2Panel2.Controls.Add(SaveBttn);
+            guna2Panel2.CustomizableEdges = customizableEdges9;
+            guna2Panel2.Dock = DockStyle.Bottom;
+            guna2Panel2.FillColor = Color.FromArgb(58, 57, 96);
+            guna2Panel2.Location = new Point(0, 640);
+            guna2Panel2.Name = "guna2Panel2";
+            guna2Panel2.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            guna2Panel2.Size = new Size(1158, 97);
+            guna2Panel2.TabIndex = 0;
+            // 
+            // CloseBttn
+            // 
+            CloseBttn.AutoRoundedCorners = true;
+            CloseBttn.BackColor = Color.Transparent;
+            CloseBttn.BorderRadius = 37;
+            customizableEdges5.TopRight = false;
+            CloseBttn.CustomizableEdges = customizableEdges5;
+            CloseBttn.DisabledState.BorderColor = Color.DarkGray;
+            CloseBttn.DisabledState.CustomBorderColor = Color.DarkGray;
+            CloseBttn.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            CloseBttn.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            CloseBttn.FillColor = Color.FromArgb(94, 104, 100);
+            CloseBttn.Font = new Font("Segoe UI", 12F);
+            CloseBttn.ForeColor = Color.Black;
+            CloseBttn.Location = new Point(942, 9);
+            CloseBttn.Name = "CloseBttn";
+            CloseBttn.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            CloseBttn.Size = new Size(182, 77);
+            CloseBttn.TabIndex = 1;
+            CloseBttn.Text = "Close";
+            CloseBttn.Click += CloseBttn_Click;
+            // 
+            // SaveBttn
+            // 
+            SaveBttn.AutoRoundedCorners = true;
+            SaveBttn.BackColor = Color.Transparent;
+            SaveBttn.BorderRadius = 37;
+            customizableEdges7.TopRight = false;
+            SaveBttn.CustomizableEdges = customizableEdges7;
+            SaveBttn.DisabledState.BorderColor = Color.DarkGray;
+            SaveBttn.DisabledState.CustomBorderColor = Color.DarkGray;
+            SaveBttn.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            SaveBttn.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            SaveBttn.FillColor = Color.FromArgb(94, 104, 109);
+            SaveBttn.Font = new Font("Segoe UI", 12F);
+            SaveBttn.ForeColor = Color.Black;
+            SaveBttn.Location = new Point(25, 9);
+            SaveBttn.Name = "SaveBttn";
+            SaveBttn.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            SaveBttn.Size = new Size(182, 77);
+            SaveBttn.TabIndex = 1;
+            SaveBttn.Text = "Save";
+            SaveBttn.Click += SaveBttn_Click;
+            // 
+            // SampleAdd
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            BackColor = Color.FromArgb(169, 191, 168);
+            ClientSize = new Size(1158, 737);
+            Controls.Add(guna2Panel2);
+            Controls.Add(guna2Panel1);
+            Font = new Font("Segoe UI", 10.125F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ForeColor = SystemColors.ControlDark;
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "SampleAdd";
+            Text = "SampleAdd";
+            guna2Panel1.ResumeLayout(false);
+            guna2Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox1).EndInit();
+            guna2Panel2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+        public Label label1;
+        public Guna.UI2.WinForms.Guna2Button SaveBttn;
+        public Guna.UI2.WinForms.Guna2Button CloseBttn;
+        public Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        public Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        public Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+    }
+}
